ce72b4529ffcb8e8f61bd4e007a8ebdaa9682a54
nurs <nursultan.tolepbergen@gmail.com>
2019-12-22 16:30:48 -0500

    FIRST COMMIT

--- src/day32/MaxMinSumArrayMethod.java
+++ src/day32/MaxMinSumArrayMethod.java
@@ -2,7 +2,7 @@ package day32;
 
 import java.util.Arrays;
 
-public class ArrayPracticeWithMethod {
+public class MaxMinSumArrayMethod {
     public static void main(String[] args) {
         printArrayItems(new int[]{1, 6, 5, 3, 12, 3});
         int[] scores = {2, 5, 8, 23, 4, 5, 6};
@@ -10,6 +10,7 @@ public class ArrayPracticeWithMethod {
         printMaxofIntArray(new int[]{1, 5, 2, 6, 33, 66, 2});
         printMinofIntArray(new int[]{5, 6, 2, 9, 7, 5, 4});
         sumOfAll(new int[]{1,2,3,4,5});
+        compare2arraySize(new String[]{"Sezgin","Senay"}, new String[]{"Araz"});
     }
 
     public static void printArrayItems(int[] nums) {
@@ -48,4 +49,14 @@ public static void sumOfAll(int[] numbers){
     }
     System.out.println("the sum of the array "+Arrays.toString(numbers)+" is: " + sum);
 }
+public static void compare2arraySize(String[] arr1, String[]arr2){
+        if(arr1.length>arr2.length){
+            System.out.println("array 1 has more");
+        }else if(arr1.length<arr2.length){
+            System.out.println("array 2 has more");
+        }else{
+            System.out.println("same");
+        }
+}
+
 }
